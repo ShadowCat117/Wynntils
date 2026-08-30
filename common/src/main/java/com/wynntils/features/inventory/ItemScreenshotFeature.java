8c8227c3034bb76641c15d7f5605ace4424fc585
Jamie <JamCal117@gmail.com>
2026-08-30 11:21:49 +0100

    port: Port tooltip rendering (Extra argument added)

--- common/src/main/java/com/wynntils/features/inventory/ItemScreenshotFeature.java
+++ common/src/main/java/com/wynntils/features/inventory/ItemScreenshotFeature.java
@@ -181,7 +181,7 @@ public class ItemScreenshotFeature extends Feature {
 
         guiGraphics.pose().pushMatrix();
         guiGraphics.pose().scale(scalew, scaleh);
-        guiGraphics.tooltip(mc.font, tooltip, 0, 0, NO_POSITIONER, tooltipStyle);
+        guiGraphics.tooltip(mc.font, tooltip, 0, 0, NO_POSITIONER, tooltipStyle, false);
         guiGraphics.pose().popMatrix();
 
         guiRenderer.render();
